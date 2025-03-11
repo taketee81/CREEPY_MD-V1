6dcb980d547ccf4c6a1bf7f6e6e712d0e01c98f0
taketee81 <161117537+taketee81@users.noreply.github.com>
2025-03-11 22:10:29 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "255697608274"
-global.ownername = "𝐷𝛥𝛮𝛮𝑌"
-global.ownernumber = '255697608274'  //owner number 2
+global.ownernomer = "23274448130"
+global.ownername = "Mr Take Tee"
+global.ownernumber = '23274448130'  //owner number 2
 //put your phone number above
 //let the owner as Danny or you can just put your name
 
